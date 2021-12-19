dcacca691eb4dec383190afbdd66448b5f280767
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2021-12-19 16:12:23 +0700

    shopping cart page add

---
+++ lib/api/shopping_cart/api_order.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:tugas_akhir/models/shopping_cart/OrderCart.dart';
+
+Future<OrderCart> fetchOrderCart() async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:8000/order_json/'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return OrderCart.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Order');
+  }
+}
---
+++ lib/models/shopping_cart/Get.dart
@@ -0,0 +1,21 @@
+class Get {
+  final int getPriceTotal;
+  final int getItemsTotal;
+
+  Get({
+    required this.getPriceTotal,
+    required this.getItemsTotal,
+  });
+
+  factory Get.fromJson(Map<String, dynamic> json) {
+    return Get(
+      getPriceTotal : json['get_price_total'],
+      getItemsTotal : json['get_items_total'],
+    );
+  }
+
+  dynamic toJson() => {
+    'get_price_total' : getPriceTotal,
+    'get_items_total' : getItemsTotal,
+  };
+}
